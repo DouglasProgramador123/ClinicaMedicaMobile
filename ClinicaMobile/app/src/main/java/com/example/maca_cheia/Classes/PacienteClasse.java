90c6818770ef012de7ac8977fa6c2941715747a3
Douglas Lopes Amora <92958927+DouglasProgramador123@users.noreply.github.com>
2021-10-25 13:56:20 -0300

    Create PacienteClasse.java

---
+++ ClinicaMobile/app/src/main/java/com/example/maca_cheia/Classes/PacienteClasse.java
@@ -0,0 +1,88 @@
+package com.example.maca_cheia.Classes;
+
+
+
+public class PacienteClasse {
+
+    private int id;
+    private String login;
+    private String senha;
+    private String nome;
+    private String cpf;
+    private String tel;
+    private String end;
+    private String cep;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
